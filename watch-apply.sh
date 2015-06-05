f13d4742aa942ef76d869cbdf72d591568e9c495
Magnus Kulke <mkulke@gmail.com>
2015-06-05 12:45:25 +0200

    Madeetcd peer configurable.

--- watch-apply.sh
+++ watch-apply.sh
@@ -1,2 +1,2 @@
-#! /bin/sh
-exec ./etcdctl exec-watch --recursive /registry/services -- ruby ./iptables-routing.rb
+#! /bin/bash
+exec ./etcdctl exec-watch --peers ${ETCD_PEER-"localhost:4001"} --recursive /registry/services -- ruby ./iptables-routing.rb
